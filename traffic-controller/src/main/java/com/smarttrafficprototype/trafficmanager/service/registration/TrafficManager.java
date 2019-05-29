0637a8106dd1d0eaa77c2eb81e1c574f67c36b42
rafaelfq <rfqueiroz.91@gmail.com>
2019-05-29 19:21:31 -0300

    Parameterizing the magic values and creating methods for steps of execution.

--- traffic-controller/src/main/java/com/smarttrafficprototype/trafficmanager/service/registration/TrafficManager.java
+++ traffic-controller/src/main/java/com/smarttrafficprototype/trafficmanager/service/registration/TrafficManager.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,8 @@ import com.smarttrafficprototype.trafficmanager.service.request.RemoteRequestSer
 
 @Component
 public class TrafficManager {
+	private static final int MILLISECONDS = 1000;
+
 	public Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@Autowired
@@ -23,55 +26,49 @@ public class TrafficManager {
 	private RemoteRequestService requestService;
 	@Autowired
 	private ServiceRegistry serviceRegistry;
-	private Integer greenSignDuration = 30;
-	private Date greenLightStartTime = new Date();
-	private Integer windowTimeCalculation = 5;
 	
+	@Value("${setup.greenSignDurationInSec}")
+	private Integer greenSignDuration = 10;
+	@Value("${setup.windowTimeCalculationInSec}")
+	private Integer windowTimeCalculation = 5;
+	@Value("${setup.minimumGreenLightDuration}")
+	private Integer minimumGreenLightDuration = 10;
+	@Value("${setup.maximumGreenLightDuration}")
+	private Integer maximumGreenLightDuration = 60;
+	@Value("${setup.maximumGreenLightDuration}")
+	private Integer trafficJunctionCycleDuration = 180;
+	
+	private Date greenLightStartTime = new Date();
 	private boolean nextLineChosen;
-
-	private int timeInSeconds;
-
+	private static int timeInSeconds;
 	private InboundTrafficLine lineMaxDensity;
 	
-	@Scheduled(initialDelay=0, fixedDelay=1000)
+	
+	
+	@Scheduled(initialDelay=0, fixedDelayString="${setup.calculationTimeDurationInMili}")
 	public void run() throws Exception {
 		triggerSensors();
 		
 		Long greenLightTimeElapsed = new Date().getTime() - greenLightStartTime.getTime();
-		Integer greenLightTimeRemaining =  greenSignDuration - (greenLightTimeElapsed.intValue() / 1000);
+		Integer greenLightTimeRemaining =  greenSignDuration - (greenLightTimeElapsed.intValue() / MILLISECONDS);
 		
 		logger.info("Green light remainging time: " + greenLightTimeRemaining + " seconds");
 		
 		trafficJunction.toString();
+		
 		if (greenLightTimeRemaining > windowTimeCalculation) {
 			return;
 		}
 		
-		if (!nextLineChosen) {
-			//busca a via de maior densidade
-			List<InboundTrafficLine> redLines = trafficJunction.getInboundLines()
-					.stream().filter((inLn) -> inLn.getTrafficLight().isRed()).collect(Collectors.toList());
-			
-			lineMaxDensity = getMaximumTrafficLine(redLines);
-			
-			//calcula o tempo de sinal verde da via
-			Integer totalDensity = redLines.stream().map((line) -> line.getSensingUnit().getResultDensity()).reduce(Integer::sum).get();
-			
-			timeInSeconds = totalDensity == 0 ? totalDensity : (lineMaxDensity.getTotalDensity() / totalDensity) * 180;
-			
-			logger.info("Calculating time of next green: (" + lineMaxDensity.getTotalDensity() + "/" + totalDensity + ") * 180 = " + timeInSeconds );
-			
-			nextLineChosen = true;
-		} 
 		
+		calculateNextTimeGreenSign();
+		
+		changeTrafficLineGreenSign(greenLightTimeRemaining);
+	}
+
+	private void changeTrafficLineGreenSign(Integer greenLightTimeRemaining) {
 		if (greenLightTimeRemaining < 1) {
-			if (timeInSeconds < 10) {
-				greenSignDuration = 10;
-			} else if (timeInSeconds > 60) {
-				greenSignDuration = 60;
-			} else {
-				greenSignDuration = timeInSeconds;
-			}
+			setGreenLightDuration();
 			
 			InboundTrafficLine greenTrafficLine = trafficJunction.getInboundLines()
 					.stream().filter((inLn) -> inLn.getTrafficLight().isGreen()).findFirst().get();
@@ -87,6 +84,58 @@ public class TrafficManager {
 			nextLineChosen = false;
 		}
 	}
+
+	private void setGreenLightDuration() {
+		if (timeInSeconds <= minimumGreenLightDuration) {
+			greenSignDuration = minimumGreenLightDuration;
+		} else if (timeInSeconds > maximumGreenLightDuration) {
+			greenSignDuration = maximumGreenLightDuration;
+		} else {
+			greenSignDuration = timeInSeconds;
+		}
+	}
+
+	private void calculateNextTimeGreenSign() {
+		if (!nextLineChosen) {
+			List<InboundTrafficLine> redLines = trafficJunction.getInboundLines()
+					.stream().filter((inLn) -> inLn.getTrafficLight().isRed()).collect(Collectors.toList());
+			
+			getIncommingDensityFromNeighborJunctions(redLines);
+			lineMaxDensity = getMaximumTrafficLine(redLines);
+			
+			int totalDensity = redLines.stream().map((line) -> line.getSensingUnit().getResultDensity()).reduce(Integer::sum).get();
+			
+			if (totalDensity > 0) {
+				Double calcTime = (lineMaxDensity.getTotalDensity().doubleValue() / totalDensity) * trafficJunctionCycleDuration;
+				timeInSeconds = calcTime.intValue();
+			} else {
+				timeInSeconds = 0;
+			}
+			
+			logger.info("Calculating time of next green: (" + lineMaxDensity.getTotalDensity() + "/" + totalDensity + ") * "+trafficJunctionCycleDuration+" = " + timeInSeconds );
+			
+			nextLineChosen = true;
+		}
+	}
+	
+	private void getIncommingDensityFromNeighborJunctions(List<InboundTrafficLine> inboundLines) {
+		for (InboundTrafficLine trafficLine : inboundLines) {
+			TrafficJunction inboundTrafficJunction = trafficLine.getInboundTrafficJunction();
+			Integer incomingDensity = 0;
+			if (inboundTrafficJunction != null) {
+				try {
+					incomingDensity = requestService.requestDensity(
+							inboundTrafficJunction.getHost(), inboundTrafficJunction.getPort(), new String[]{trafficJunction.getJunctionKey()});
+					
+					logger.info("Incoming density: " + incomingDensity + " from " + trafficLine.getInboundTrafficJunction().getJunctionKey());
+				} catch (Exception ex) {
+					incomingDensity = 0;
+				}
+			}
+			
+			trafficLine.setIncomingDensity(incomingDensity);
+		}
+	}
 	
 	private void triggerSensors() {
 		for (Sensor sensor : serviceRegistry.getSensors()) {
@@ -97,19 +146,6 @@ public class TrafficManager {
 	private InboundTrafficLine getMaximumTrafficLine(List<InboundTrafficLine> inboundLines) {
 		InboundTrafficLine lineMaxDensity = null;
 		for (InboundTrafficLine trafficLine : inboundLines) {
-			
-			TrafficJunction inboundTrafficJunction = trafficLine.getInboundTrafficJunction();
-			Integer incomingDensity = 0;
-			if (inboundTrafficJunction != null) {
-				incomingDensity = requestService.requestDensity(
-						inboundTrafficJunction.getHost(), inboundTrafficJunction.getPort(), new String[]{trafficJunction.getJunctionKey()});
-				
-				logger.info("Incoming density: " + incomingDensity + " from " + trafficLine.getInboundTrafficJunction().getJunctionKey());
-			}
-			
-			trafficLine.setIncomingDensity(incomingDensity);
-			
-			//incrementar densidade com o valor obtido da OutboundTrafficLine
 			if (lineMaxDensity == null || trafficLine.getTotalDensity() > lineMaxDensity.getTotalDensity()) {
 				lineMaxDensity = trafficLine;
 			}
