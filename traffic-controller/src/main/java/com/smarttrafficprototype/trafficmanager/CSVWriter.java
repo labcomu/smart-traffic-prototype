5c35b8f866d2709a13a9b6876457fb8fe539d990
rafaelfq <rfqueiroz.91@gmail.com>
2019-05-30 17:52:02 -0300

    Date format for result.

--- traffic-controller/src/main/java/com/smarttrafficprototype/trafficmanager/CSVWriter.java
+++ traffic-controller/src/main/java/com/smarttrafficprototype/trafficmanager/CSVWriter.java
@@ -3,6 +3,7 @@ package com.smarttrafficprototype.trafficmanager;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,19 +14,23 @@ public class CSVWriter {
 	@Autowired
 	private ExecutionCyclesRepository cyclesRepository;
 	
+	private static SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSSS");
+	
 	public void writeCSV() {
 		try (PrintWriter writer = new PrintWriter(new File("result.csv"))) {
 
 			StringBuilder sb = new StringBuilder();
 			sb.append("id,");
 			sb.append("timeStamp,");
-			sb.append("duration");
+			sb.append("executedAt,");
+			sb.append("duration,");
 			sb.append("result,");
 			sb.append('\n');
 			
 			for (ExecutionCycle cycle : cyclesRepository.getAll()) {
 				sb.append(cycle.getId()).append(',');
 				sb.append(cycle.getMoment().getTime()).append(',');
+				sb.append(formatter.format(cycle.getMoment())).append(',');
 				sb.append(cycle.getDuration()).append(',');
 				sb.append(cycle.getClassification().toString()).append('\n');
 			}
